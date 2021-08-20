5263fdb14caf831db2f69574cfedc26f0dfda5f7
Daniel Benica <danielbenica7@gmail.com>
2021-08-20 14:18:03 -0300

    Implmentation of tehe final states

--- gas_pump.vhd
+++ gas_pump.vhd
@@ -3,6 +3,7 @@
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 
 
@@ -24,26 +25,32 @@ architecture behaviour of gas_pump is
 type states is (idle, payment, fuel_selection, fueling, comparator, give_change);
 type fuel is array (0 to 2) of std_logic_vector(7 downto 0);
 signal current_state : states := idle;	
-variable current_credit : std_logic_vector(7 downto 0); 	
-variable fuel_price : fuel;
-variable selected_fuel : std_logic_vector(7 downto 0);
 
-fuel_price(0) := x"06";
-fuel_price(1) := x"05";
-fuel_price(2) := x"04";
+	
 	begin
 		
-		shrek : process(clock, btn_continue, fuel_type, credit_input, change, pump, current_state)
-					
+		shrek : process(clock, btn_continue, fuel_type, credit_input, current_state)
+		
+				variable current_credit : std_logic_vector(7 downto 0); 	
+				variable fuel_price : fuel;
+				variable selected_fuel : std_logic_vector(7 downto 0);
+				variable flow_rate : integer := 20;
+				variable verification_period : time := 250 ms;
+				
+				begin	
+				
 					if(rising_edge(clock)) then 
 						case current_state is 
 							
 							
 							when idle => 
+								fuel_price(0) := "00001010";
+								fuel_price(1) := "00000101";
+								fuel_price(2) := "00000100";
 								change <= x"00";
-								selected_fuel := "11";
-								pump <= '0'
-								if (btn_continue) then 
+								selected_fuel := x"00";
+								pump <= '0';
+								if (btn_continue = '1') then 
 									current_state <= payment;
 								end if;
 								
@@ -51,7 +58,7 @@ fuel_price(2) := x"04";
 							
 							when payment =>
 								current_credit := credit_input;
-								if(btn_continue) then	
+								if(btn_continue = '1') then	
 									current_state <= fuel_selection;
 								end if;
 						
@@ -68,11 +75,40 @@ fuel_price(2) := x"04";
 									selected_fuel := fuel_price(2);
 									
 								end if;
-								if(btn_continue) then 
+								if(btn_continue = '1' and selected_fuel /= x"11") then 
 									current_state <= fueling;
 								end if;
-		
-		
+								
+								
+								
+							when fueling =>
+								pump <= '1';
+								wait for verification_period;
+								current_state <= comparator;
+							
+							
+							
+							when comparator => 
+								--current_credit := std_logic_vector(to_integer(unsigned(current_credit)) - ((flow_rate*to_integer(verification_period)*to_integer(unsigned(selected_fuel)))/1000);
+								if(btn_continue = '1') then
+									pump <= '0';
+									current_state <= give_change;
+									
+								elsif(to_integer(unsigned(current_credit)) <= 0) then 
+									pump <= '0';
+									current_state <= idle;
+									
+								else
+									current_state <= fueling;
+									
+								end if;
+								
+								
+								
+							when give_change =>
+								change <= current_credit;
+								current_state <= idle;
+						end case;
 					end if;
 					
 
